d6d3ccfc22895130b4a06257f5dc0805872ac0d5
Riccardo Briccola <richi2000.tablet@gmail.com>
2018-10-10 20:07:24 +0200

    First commit with Visual studio project files and forms

--- Windows10LockScreens/Program.cs
+++ Windows10LockScreens/Program.cs
@@ -16,7 +16,7 @@ namespace Windows10LockScreens
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new mainFrm());
         }
     }
 }
