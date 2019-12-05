0226d1e0311f803b361a0e4dff2249832402ea71
Riccardo Briccola <richi2000.tablet@gmail.com>
2019-12-05 10:12:14 +0100

    Added resolution filter

--- Windows10LockScreens/mainForm.Designer.cs
+++ Windows10LockScreens/mainForm.Designer.cs
@@ -32,16 +32,23 @@
             this.previewPictureBox = new System.Windows.Forms.PictureBox();
             this.filesList = new System.Windows.Forms.ListView();
             this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.widthMinResolutionBox = new System.Windows.Forms.NumericUpDown();
+            this.heightMinResolutionBox = new System.Windows.Forms.NumericUpDown();
+            this.filterBox = new System.Windows.Forms.CheckBox();
             ((System.ComponentModel.ISupportInitialize)(this.previewPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.widthMinResolutionBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.heightMinResolutionBox)).BeginInit();
             this.SuspendLayout();
             // 
             // saveBtn
             // 
             this.saveBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.saveBtn.Enabled = false;
-            this.saveBtn.Location = new System.Drawing.Point(722, 423);
+            this.saveBtn.Location = new System.Drawing.Point(963, 521);
+            this.saveBtn.Margin = new System.Windows.Forms.Padding(4);
             this.saveBtn.Name = "saveBtn";
-            this.saveBtn.Size = new System.Drawing.Size(75, 23);
+            this.saveBtn.Size = new System.Drawing.Size(100, 28);
             this.saveBtn.TabIndex = 0;
             this.saveBtn.Text = "Save as..";
             this.saveBtn.UseVisualStyleBackColor = true;
@@ -51,10 +58,11 @@
             // 
             this.previewPictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.previewPictureBox.Location = new System.Drawing.Point(234, 6);
+            this.previewPictureBox.Location = new System.Drawing.Point(312, 7);
+            this.previewPictureBox.Margin = new System.Windows.Forms.Padding(4);
             this.previewPictureBox.Name = "previewPictureBox";
-            this.previewPictureBox.Size = new System.Drawing.Size(563, 440);
-            this.previewPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.previewPictureBox.Size = new System.Drawing.Size(751, 506);
+            this.previewPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.previewPictureBox.TabIndex = 1;
             this.previewPictureBox.TabStop = false;
             // 
@@ -62,9 +70,11 @@
             // 
             this.filesList.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left)));
-            this.filesList.Location = new System.Drawing.Point(7, 6);
+            this.filesList.HideSelection = false;
+            this.filesList.Location = new System.Drawing.Point(9, 7);
+            this.filesList.Margin = new System.Windows.Forms.Padding(4);
             this.filesList.Name = "filesList";
-            this.filesList.Size = new System.Drawing.Size(221, 440);
+            this.filesList.Size = new System.Drawing.Size(293, 541);
             this.filesList.TabIndex = 2;
             this.filesList.UseCompatibleStateImageBehavior = false;
             this.filesList.View = System.Windows.Forms.View.Tile;
@@ -72,23 +82,91 @@
             // 
             // saveFileDialog1
             // 
+            this.saveFileDialog1.DefaultExt = "jpg";
+            this.saveFileDialog1.FileName = "Backgroud";
             this.saveFileDialog1.Filter = "Immagine JPG|*.jpg|Tutti i file|*.*";
             this.saveFileDialog1.Title = "Save Wallpaper";
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(309, 523);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(209, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Mininum Resolution (WxH - px): ";
+            // 
+            // widthMinResolutionBox
+            // 
+            this.widthMinResolutionBox.Enabled = false;
+            this.widthMinResolutionBox.Location = new System.Drawing.Point(519, 522);
+            this.widthMinResolutionBox.Maximum = new decimal(new int[] {
+            5000,
+            0,
+            0,
+            0});
+            this.widthMinResolutionBox.Name = "widthMinResolutionBox";
+            this.widthMinResolutionBox.Size = new System.Drawing.Size(67, 22);
+            this.widthMinResolutionBox.TabIndex = 4;
+            this.widthMinResolutionBox.Value = new decimal(new int[] {
+            600,
+            0,
+            0,
+            0});
+            this.widthMinResolutionBox.ValueChanged += new System.EventHandler(this.widthMinResolutionBox_ValueChanged);
+            // 
+            // heightMinResolutionBox
+            // 
+            this.heightMinResolutionBox.Enabled = false;
+            this.heightMinResolutionBox.Location = new System.Drawing.Point(592, 522);
+            this.heightMinResolutionBox.Maximum = new decimal(new int[] {
+            5000,
+            0,
+            0,
+            0});
+            this.heightMinResolutionBox.Name = "heightMinResolutionBox";
+            this.heightMinResolutionBox.Size = new System.Drawing.Size(67, 22);
+            this.heightMinResolutionBox.TabIndex = 5;
+            this.heightMinResolutionBox.Value = new decimal(new int[] {
+            600,
+            0,
+            0,
+            0});
+            this.heightMinResolutionBox.ValueChanged += new System.EventHandler(this.heightMinResolutionBox_ValueChanged);
+            // 
+            // filterBox
+            // 
+            this.filterBox.AutoSize = true;
+            this.filterBox.Location = new System.Drawing.Point(673, 523);
+            this.filterBox.Name = "filterBox";
+            this.filterBox.Size = new System.Drawing.Size(90, 21);
+            this.filterBox.TabIndex = 6;
+            this.filterBox.Text = "Use Filter";
+            this.filterBox.UseVisualStyleBackColor = true;
+            this.filterBox.CheckedChanged += new System.EventHandler(this.filterBox_CheckedChanged);
+            // 
             // mainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.filterBox);
+            this.Controls.Add(this.heightMinResolutionBox);
+            this.Controls.Add(this.widthMinResolutionBox);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.saveBtn);
             this.Controls.Add(this.filesList);
             this.Controls.Add(this.previewPictureBox);
-            this.MinimumSize = new System.Drawing.Size(500, 400);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MinimumSize = new System.Drawing.Size(661, 481);
             this.Name = "mainForm";
             this.Text = "Windows10LockScreen";
             this.Load += new System.EventHandler(this.mainFrm_Load);
             ((System.ComponentModel.ISupportInitialize)(this.previewPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.widthMinResolutionBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.heightMinResolutionBox)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -98,6 +176,10 @@
         private System.Windows.Forms.PictureBox previewPictureBox;
         private System.Windows.Forms.ListView filesList;
         private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown widthMinResolutionBox;
+        private System.Windows.Forms.NumericUpDown heightMinResolutionBox;
+        private System.Windows.Forms.CheckBox filterBox;
     }
 }
 
--- Windows10LockScreens/mainForm.cs
+++ Windows10LockScreens/mainForm.cs
@@ -35,16 +35,7 @@ namespace Windows10LockScreens
 
         private void mainFrm_Load(object sender, EventArgs e)
         {
-            //Now i load files to the listView
-            filesList.Items.Clear();
-            string[] files = Directory.GetFiles(wallpapersPath);
-            foreach (string file in files)
-            {
-                string fileName = Path.GetFileNameWithoutExtension(file);
-                ListViewItem item = new ListViewItem(fileName);
-                item.Tag = file;
-                filesList.Items.Add(item);
-            }
+            ReloadImages();
         }
 
 
@@ -70,6 +61,65 @@ namespace Windows10LockScreens
                 if (saveFileDialog1.ShowDialog() == DialogResult.OK)
                 {
                     File.Copy(selectedImagePath, saveFileDialog1.FileName);
+                }
+            }
+        }
+
+        private void filterBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if(filterBox.Checked)
+            {
+                widthMinResolutionBox.Enabled = true;
+                heightMinResolutionBox.Enabled = true;
+                ReloadImages(Convert.ToInt32(widthMinResolutionBox.Value),Convert.ToInt32(heightMinResolutionBox.Value));
+            }
+            else
+            {
+                widthMinResolutionBox.Enabled = false;
+                heightMinResolutionBox.Enabled = false;
+                ReloadImages();
+            }
+        }
+
+
+        private void heightMinResolutionBox_ValueChanged(object sender, EventArgs e)
+        {
+            ReloadImages(Convert.ToInt32(widthMinResolutionBox.Value), Convert.ToInt32(heightMinResolutionBox.Value));
+        }
+
+        private void widthMinResolutionBox_ValueChanged(object sender, EventArgs e)
+        {
+            ReloadImages(Convert.ToInt32(widthMinResolutionBox.Value), Convert.ToInt32(heightMinResolutionBox.Value));
+        }
+
+
+        void ReloadImages(int minW = -1, int minH = -1)
+        {
+            string fileName;
+            string[] files;
+            ListViewItem item;
+            //Now i load files to the listView
+            filesList.Items.Clear();
+            files = Directory.GetFiles(wallpapersPath);
+            foreach (string file in files)
+            {
+                if (minW > 0 && minH > 0) //Then i filter files with resolution
+                {
+                    var img = Image.FromFile(file);
+                    if (img.Width >= minW && img.Height >= minH)
+                    {
+                        fileName = Path.GetFileNameWithoutExtension(file);
+                        item = new ListViewItem(fileName);
+                        item.Tag = file;
+                        filesList.Items.Add(item);
+                    }
+                }
+                else
+                {
+                    fileName = Path.GetFileNameWithoutExtension(file);
+                    item = new ListViewItem(fileName);
+                    item.Tag = file;
+                    filesList.Items.Add(item);
                 }
             }
         }
